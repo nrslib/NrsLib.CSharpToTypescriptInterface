f15d4c2df3fc2b5563f3c271ef675de0bb9676f0
srn <masanobu.naruse@gmail.com>
2018-07-08 21:02:03 +0900

    delete log for test

--- CSharpToTypescriptInterface/ItemExtractor/Property/DefaultPropertyExtractor.cs
+++ CSharpToTypescriptInterface/ItemExtractor/Property/DefaultPropertyExtractor.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace CSharpToTypescriptInterface.ItemExtractor.Property {
     class DefaultPropertyExtractor : IPropertyExtractor {
         public bool IsSatisfiedBy(PropertyInfo propertyInfo)
         {
-            var test = propertyInfo.GetMethod.IsPublic;
-            if (!test)
-            {
-                Console.WriteLine();
-            }
-
             return propertyInfo.GetMethod.IsPublic;
         }
     }
